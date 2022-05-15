c47a005bae58fb5c43bf00eea964caf31a2fda5d
Cristopher0206 <cristopher.perez@epn.edu.ec>
2022-05-15 18:04:07 -0500

    Primer incremento

---
+++ ProyDistrEspinoza1.1/src/proydistrespinoza1/pkg1/Main.java
@@ -0,0 +1,166 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proydistrespinoza1.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Cris
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Empleado emp = new Empleado();
+        int bandera = 0;
+        System.out.println("Bienvenido al sistema de gestión de empleados de la Distribuidora Espinoza");
+        while (bandera == 0) {
+            System.out.println("Seleccione una opción");
+            System.out.println("1. Registro de empleados");
+            System.out.println("2. Rotación de empleados");
+            System.out.println("3. Salir\n");
+
+            String sEntrada = "";
+            Scanner entrada = new Scanner(System.in);
+            sEntrada = entrada.nextLine();
+            int iEntrada = Integer.parseInt(sEntrada);
+
+            switch (iEntrada) {
+                case 1:
+                    String codigo = "";
+                    String sNombre = "";
+                    String sApellido = "";
+                    String sCargo = "";
+                    String ultSucursal = "";
+                    String sDireccion = "";
+                    String sCorreo = "";
+                    String sTelefono = "";
+                    // Registrar Nombre
+                    boolean flag = false;
+                    while (flag == false) {
+                        System.out.print("Nombre del empleado: ");
+                        Scanner entradaNombre = new Scanner(System.in);
+                        sNombre = entradaNombre.nextLine();
+                        // verificar nombre
+                        if (emp.esNombreValido(sNombre)) {
+                            flag = true;
+                        } else {
+                            System.out.println("Su nombre no puede contener números ni caracteres especiales");
+                        }
+                    }
+                    flag = false;
+                    // Registrar Apellido
+                    while (flag == false) {
+                        System.out.print("Apellido del empleado: ");
+                        Scanner entradaApellido = new Scanner(System.in);
+                        sApellido = entradaApellido.nextLine();
+                        // verificar nombre
+                        if (emp.esApellidoValido(sApellido)) {
+                            flag = true;
+                        } else {
+                            System.out.println("Su apellido no puede contener números ni caracteres especiales");
+                        }
+                    }
+                    flag = false;
+                    // Seleccionar cargo / generar código
+                    codigo = "";
+                    while (flag == false) {
+                        System.out.println("Seleccione el cargo del empleado");
+                        System.out.println("a. Vendedor");
+                        System.out.println("b. Contador");
+                        System.out.println("c. Encargado de Bodega");
+                        Scanner entradaCargo = new Scanner(System.in);
+                        sCargo = entradaCargo.nextLine();
+
+                        switch (sCargo) {
+                            case "a":
+                                sCargo = "Vendedor";
+                                codigo += "VD";
+                                codigo += "-";
+                                codigo += emp.generarDigitos();
+                                flag = true;
+                                break;
+                            case "b":
+                                sCargo = "Contador";
+                                codigo += "CT";
+                                codigo += "-";
+                                codigo += emp.generarDigitos();
+                                flag = true;
+                                break;
+                            case "c":
+                                sCargo = "Encargado de Bodega";
+                                codigo += "EB";
+                                codigo += "-";
+                                codigo += emp.generarDigitos();
+                                flag = true;
+                                break;
+                            default:
+                                //System.out.println("Opcion no válida, vuelva a intentarlo");
+                                break;
+                        }
+                    }
+                    flag = false;
+                    // Generar Última Sucursal
+                    int numRandom = (int) (Math.random() * 3 + 1);
+                    ultSucursal = String.valueOf(numRandom);
+                    // Registrar Dirección
+                    while (flag == false) {
+                        System.out.print("Dirección del empleado: ");
+                        Scanner entradaDireccion = new Scanner(System.in);
+                        sDireccion = entradaDireccion.nextLine();
+
+                        if (emp.esDireccionValida(sDireccion)) {
+                            flag = true;
+                        } else {
+                            System.out.println("Su direccion no puede contener caracteres especiales");
+                        }
+                    }
+                    flag = false;
+                    // Registrar Correo Electrónico
+                    while (flag == false) {
+                        System.out.print("Correo Electrónico del empleado: ");
+                        Scanner entradaCorreo = new Scanner(System.in);
+                        sCorreo = entradaCorreo.nextLine();
+                        if (emp.esCorreoValido(sCorreo)) {
+                            flag = true;
+                        } else {
+                            System.out.println("Correo inválido, vuelva a ingresarlo");
+                        }
+                    }
+                    flag = false;
+                    // Registrar Número de Teléfono
+                    while (flag == false) {
+                        System.out.print("Número de teléfono del empleado: ");
+                        Scanner entradaTelefono = new Scanner(System.in);
+                        sTelefono = entradaTelefono.nextLine();
+                        if (emp.esTelefonoValido(sTelefono)) {
+                            flag = true;
+                        } else {
+                            System.out.println("Número de teléfono inválido, vuelva a ingresarlo");
+                        }
+                    }
+                    
+                    System.out.println("\n");
+
+                    Empleado newEmpl = new Empleado(codigo, sNombre, sApellido, sCargo, ultSucursal, sDireccion, sCorreo, sTelefono);
+                    System.out.println(newEmpl.toString());
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                    bandera = 1;
+                    break;
+            }
+
+        }
+    }
+
+}
---
+++ ProyDistrEspinoza1.1/src/proydistrespinoza1/pkg1/Sucursal.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proydistrespinoza1.pkg1;
+
+/**
+ *
+ * @author Cris
+ */
+public class Sucursal {
+    private final int tamaño = 13;
+    private int numSucursal;
+    private String[] arregloSucursal;
+    
+    // Constructor
+    public Sucursal(int numSucursal){
+        this.numSucursal = numSucursal;
+        arregloSucursal = new String[tamaño];
+        for(int i = 0; i < arregloSucursal.length; i++){
+            arregloSucursal[i] = "";
+        }
+    }
+    
+    public void insertarCodigo(String codigo){
+        for(int i = 0; i < this.arregloSucursal.length; i++){
+            if(this.arregloSucursal[i].equals("")){
+                arregloSucursal[i] = codigo;
+                break;
+            }
+        }
+    }
+    
+    public void borrarCodigo(String info){
+        for(int i = 0; i < arregloSucursal.length; i++){
+            if(arregloSucursal[i].equals(info)){
+                arregloSucursal[i] = "";
+                break;
+            }
+        }
+    }
+    
+    public String consultarEmpleado(String info){
+        for(int i = 0; i < arregloSucursal.length; i++){
+            if(arregloSucursal[i].equals(info)){
+                return info; 
+            }
+        }
+        return "El empleado "+info+" no se encuentra en la sucursal "+this.numSucursal;
+    }
+    
+    /*public String mostrarEstructura(ListaDEnlazada lista){
+        String texto = "";
+        texto += "Los siguientes empleados trabajan en la sucursal "+this.numSucursal+"\n";
+        for(int i = 0; i < this.arreglo.length; i++){
+            if (!this.arreglo[i].equals("")){
+                texto += "- " + this.arreglo[i] + ": " +lista.obtenerEmpleado(this.arreglo[i])+"\n";
+            }
+        }
+        return texto;
+    }*/
+    
+    public void vaciarArreglo(){
+        for(int i = 0; i < 13; i++){
+            this.arregloSucursal[i] = "";
+        }
+    }
+    
+    public boolean estaVacio(){
+        if(!this.arregloSucursal[0].equals("")){
+            return false;
+        }
+        return true;
+    }
+}
